1189021231814f578a92402e65cd0f1a5c064c04
Benjamin Amos <benjamin.amos11@gmail.com>
2026-07-29 13:18:05 +0100

    build: filter repositories to fetch only what is needed

--- cr-terasology/build.gradle.kts
+++ cr-terasology/build.gradle.kts
@@ -8,15 +8,28 @@ plugins {
     idea
 }
 
+// We use both Maven Central and our own Artifactory instance, which contains module builds, extra libs, and so on
 repositories {
-    maven {
-        name = "Terasology Artifactory"
-        url = uri("https://artifactory.terasology.io/artifactory/virtual-repo-live")
+    mavenCentral {
+        content {
+            // This is first choice for most java dependencies, but assume we'll need to check our
+            // own repository for things from our own organization.
+            // (This is an optimization so gradle doesn't try to find our hundreds of modules in 3rd party repos)
+            excludeGroupByRegex("org.terasology(..+)?")
+        }
     }
-    mavenCentral()
+    // JBoss Maven Repository requried to fetch `org.jpastebin` dependency for CrashReporter
+    // https://developer.jboss.org/docs/DOC-11377
     maven {
         name = "JBoss Public Maven Repository Group"
         url = uri("https://repository.jboss.org/nexus/content/repositories/public/")
+        content {
+            includeModule("org", "jpastebin")
+        }
+    }
+    maven {
+        name = "Terasology Artifactory"
+        url = uri("https://artifactory.terasology.io/artifactory/virtual-repo-live")
     }
 }
 
